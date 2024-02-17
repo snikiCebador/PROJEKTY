8e8b5e38bc7e12b006bc145a39b598623e69e83e
snikiCebador <rafal.cecotka@o2.pl>
2024-02-17 10:54:16 +0100

    refresh

--- rafalcecotka-projekty git/js.js
+++ rafalcecotka-projekty git/js.js
@@ -107,7 +107,19 @@ else if (chosen_skill === "VBA"){
 	+ 'VBA znam w zakresie podstawowym do średniego. W ramach VBA dokonałem kilku projektów prowadzonych na własny użytek oraz do obsługi procesów wewnętrznych w miejscu zatrudnienia.'
 	+ '<br><br>'
 	+ '<i style="font-size:14px;">Po kliknięciu w przycisk zostanie uruchomiona strona z projektami używającymi <b>'+ chosen_skill + '</b></i>';
-		}		
+		}
+else if (chosen_skill === "MachineLearning"){
+	chosen_skill = 
+	'<b style="font-size:20px;">' + chosen_skill + '</b> <br>'
+	+ '<br>'
+	+ '<span style="font-size:16px;float:left;line-height:22px;"> <b>Poziom: </b> </span> 	<span style="font-size:16px;float:right;line-height:22px;" >' + get_mark(2) + '</span> <br>'
+	+ '<div style="clear:both;"> </div>'
+	+ '<span style="font-size:16px;float:left;line-height:22px;"> <b>Doświadczenie: </b> </span> 	<span style="font-size:16px;float:right;line-height:22px;" > ' + get_mark(1) + ' </span> <br>'
+	+ '<div style="clear:both;"> <br> </div>'
+	+ 'ML znam w zakresie podstawowym do średniego. W ramach ML dokonałem kilku projektów prowadzonych na własny użytek.'
+	+ '<br><br>'
+	+ '<i style="font-size:14px;">Po kliknięciu w przycisk zostanie uruchomiona strona z projektami używającymi <b>'+ chosen_skill + '</b></i>';
+		}
 document.getElementById("bottom_content_coll").style.display="none";
 document.getElementById("bottom_content_coll_add").style.display="block";
 document.getElementById("detail_msg").innerHTML = chosen_skill;
